d042e542bbff83d62e6340b7c1e63d3070ef6f15
Jonbo <jon.bosque.hernando@gmail.com>
2017-10-27 17:07:20 +0200

    Add headers so that they can be used in the listbox as column names

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 class Client:
+    str_header = ['Nombre', 'Apellidos', 'DNI', 'Tlf 1', 'Tlf 2', 'Tlf 3', 'e-mail', 'ID Cliente']
+
     def __init__(self, name: str, surname: str, id_card: str, phone1: str, phone2: str, phone3: str, email: str,
-                 client_num: str):
+                 client_id: str):
         self.name = name
         self.surname = surname
         self.id_card = id_card
@@ -8,15 +10,19 @@ class Client:
         self.phone2 = phone2
         self.phone3 = phone3
         self.email = email
-        self.client_num = int(client_num)
+        self.client_id = int(client_id)
+
 
     def __str__(self):
         ret_string = ';'.join(
-            [self.name, self.surname, self.id_card, self.phone1, self.phone2, self.phone3, self.email, str(self.client_num)])
+            [self.name, self.surname, self.id_card, self.phone1, self.phone2, self.phone3, self.email, str(self.client_id)])
         return ret_string
 
 
+
 class Alumn(Client):
+    str_header = Client.str_header + ['Domicilia (1/0)', 'Cuenta Bancaria', 'Periodo Pago', 'Grupos']
+
     def __init__(self, name: str, surname: str, id_card: str, phone1: str, phone2: str, phone3: str, email: str,
                  client_num: str, pay_bank: str, bank_acc: str, pay_period: str, groups: str):
         super().__init__(name, surname, id_card, phone1, phone2, phone3, email, client_num)
@@ -24,6 +30,7 @@ class Alumn(Client):
         self.bank_acc = bank_acc
         self.pay_period = pay_period
         self.groups = eval(groups)  # set
+
 
     def __str__(self):
         ret_string = ';'.join([super().__str__(),str(self.pay_bank),self.bank_acc,self.pay_period,str(self.groups)])
@@ -39,7 +46,7 @@ class Patient(Client):
 
 
 def check_integrity_clients_list(clients_list):
-    get_id = lambda x : x.client_num
+    get_id = lambda x : x.client_id
     map_ids = map(get_id, clients_list)
     list_ids = list(map_ids)
     if len(set(list_ids)) < len(list_ids):
--- group.py
+++ group.py
@@ -1,18 +1,28 @@
 class Group:
-    def __init__(self, price: str, time_start: str, time_end: str, name_teacher: str, name_activity: str,
-                 limit_members: str, members: str, group_num: str):
-        self.name_teacher = name_teacher
+    str_header = ['Actividad', 'Monitor/a', 'Dias', 'Inicio', 'Final', 'Precio', 'Limite de Miembros', 'Miembros',
+                  'ID Grupo']
+
+    def __init__(self, name_activity, name_teacher, days, time_start, time_end, price, limit_members, members,
+                 group_id):
         self.name_activity = name_activity
+        self.name_teacher = name_teacher
+        self.days = eval(days)
+        self.time_start = int(time_start)
+        self.time_end = int(time_end)
         self.price = float(price)
-        self.time_start = time_start
-        self.time_end = time_end
-        self.limit_members = limit_members
+        self.limit_members = int(limit_members)
         self.members = eval(members)  # set
-        self.group_num = int(group_num)
+        self.group_id = int(group_id)
+
+    def __str__(self):
+        ret_string = ';'.join(
+            [self.name_activity, self.name_teacher, str(self.days), str(self.time_start), str(self.time_end),
+             str(self.price), str(self.limit_members), str(self.members), str(self.group_id)])
+        return ret_string
 
 
 def check_integrity_groups_list(groups_list):
-    get_id = lambda x : x.group_num
+    get_id = lambda x: x.group_id
     map_ids = map(get_id, groups_list)
     list_ids = list(map_ids)
     if len(set(list_ids)) < len(list_ids):
