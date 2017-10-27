4966d5fad495adc377c82682b1cc65a71a551bc5
Jonbo <jon.bosque.hernando@gmail.com>
2017-10-27 17:04:42 +0200

    Add input/output for groups lists

--- data_io.py
+++ data_io.py
@@ -1,5 +1,5 @@
 import inspect
-
+import group as gr
 
 def load_clients(file_clients, client_type):
     with open(file_clients) as file:
@@ -15,9 +15,15 @@ def write_clients(file_clients, clients_list, client_type):
         file.write('\n'.join(map(str, clients_list)))
 
 
-def load_groups(file_groups, group_type):
-    pass
+def load_groups(file_groups):
+    with open(file_groups) as file:
+        column_names = file.readline()
+        groups_list = [gr.Group(*line.rstrip('\n').split(';')) for line in file]
+    return groups_list
 
 
-def write_groups(file_groups, groups_list, group_type):
-    pass
+def write_groups(file_groups, groups_list):
+    header = ';'.join(inspect.getfullargspec(gr.Group).args[1:])
+    with open(file_groups, 'w') as file:
+        file.write(header+'\n')
+        file.write('\n'.join(map(str, groups_list)))
