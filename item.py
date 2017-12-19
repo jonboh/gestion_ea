494b54b5bd38cd4738229b2212824d83fc7a0d2a
Jonbo <jon.bosque.hernando@gmail.com>
2017-12-19 16:24:48 +0100

    add __str__() modify internal type of prices and add id

--- item.py
+++ item.py
@@ -3,19 +3,23 @@ class Item:
                    'Precio Compra', 'PVP']
     default_header_map = [1 for _ in tree_header]
 
-    def __init__(self, name='', quantity=0, distributor='', provider='', ref_dist='', ref_ea='', price_buy=0,
-                 price_pvp=0):
+    def __init__(self, name='', quantity=0, distributor='', provider='', ref_dist='', ref_ea='', price_buy='0',
+                 price_pvp='0', item_id='-1'):
         self.name = name
         self.quantity = quantity
         self.distributor = distributor
         self.provider = provider
         self.ref_dist = ref_dist
         self.ref_ea = ref_ea
-        self.price_buy = price_buy
-        self.price_pvp = price_pvp
+        self.price_buy = float(price_buy)
+        self.price_pvp = float(price_pvp)
+        self.id = int(item_id)
 
     def __str__(self):
-        pass
+        ret_string = ';'.join(
+            [self.name, self.quantity, self.distributor, self.provider, self.ref_dist, self.ref_ea, str(self.price_buy),
+             str(self.price_pvp), str(self.id)])
+        return ret_string
 
     def display(self):
         pass
